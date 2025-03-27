3dec5e890d5b9a8397f7102ccd2f6b50ab264a06
Tigran <41443306+septoon@users.noreply.github.com>
2025-03-27 22:57:23 +0300

    fix img bg

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,7 +42,6 @@ export default function RootLayout({
           <PrimeReactProvider>
             <Header />
             {children}
-            {/* <CartIcon /> */}
           </PrimeReactProvider>
         </Providers>
         <ChatButton />

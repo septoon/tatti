42fa7c3453d46521db71f82f198d20751a7f942f
Tigran <41443306+septoon@users.noreply.github.com>
2025-04-13 19:52:52 +0300

    change form

--- app/components/Form/Form.tsx
+++ app/components/Form/Form.tsx
@@ -25,126 +25,108 @@ const Form: React.FC<FormModalProps> = ({ onFormClose }) => {
   const isBtnDisabled = name.length < 2 || phone.length < 16;
 
   return (
-    <div 
-      className="fixed inset-0 bg-black/60 flex items-center pt-10 justify-center z-50"
-      onClick={(e) => {
-        if (e.target === e.currentTarget) {
-          onFormClose();
-        }
-      }}
-    >
-      <div
-          className="bg-[#151515] text-white max-w-[90%] md:max-w-lg rounded-lg shadow-lg h-auto overflow-y-scroll max-h-[525px] md:w-auto
-            w-full pb-6 z-99999"
-          onClick={(e) => e.stopPropagation()}
-        >
-        <div className='my-2 flex justify-end p-3'>
-          <button
-            className="text-2xl text-gray-500 cursor-pointer"
-            onClick={onFormClose}
-          >
-            <IoIosCloseCircle />
-          </button>
-        </div>
-        <h2 className="text-xl px-6 font-bold mb-4">Заполните форму, и мы вам перезвоним!</h2>
-        <div className='w-full h-full px-6'>
-          <div className="mb-4">
-            <label className="block text-sm font-medium text-gray-700 mb-2">
-              Дата *
-            </label>
-            <div className="mb-4 flex items-center gap-4">
-              <div className="flex-1">
-                <div className="flex">
-                  <div className="pl-2 w-[150px] flex items-center">
-                    {date ? (
-                      <p className="text-white">{formatSelectedDate(date)}</p>
-                    ) : (
-                      <p className="text-gray-500">Выберите дату</p>
-                    )}
-                  </div>
-                  <Calendar
-                    id="datePicker"
-                    placeholder="Выберите дату"
-                    value={date}
-                    onChange={(e) => setDate(e.value ?? null)}
-                    showIcon
-                    locale="ru"
-                    dateFormat="dd MM yy"
-                  />
-                </div>
-              </div>
+    <div
+    className="bg-[#151515] text-white md:max-w-lg rounded-lg shadow-lg h-auto overflow-y-scroll md:w-auto
+      w-full pb-6"
+    onClick={(e) => e.stopPropagation()}
+  >
+  <div className='w-full h-full px-6'>
+    <div className="mb-4">
+      <label className="block text-sm font-medium text-gray-700 mb-2">
+        Дата *
+      </label>
+      <div className="mb-4 flex items-center gap-4">
+        <div className="flex-1">
+          <div className="flex">
+            <div className="pl-2 w-[150px] flex items-center">
+              {date ? (
+                <p className="text-white">{formatSelectedDate(date)}</p>
+              ) : (
+                <p className="text-gray-500">Выберите дату</p>
+              )}
             </div>
-
-            <div className="mb-4">
-              <label className="block text-sm font-medium text-gray-700 mb-2">
-                Количество персон и бюджет *
-              </label>
-              <input
-                type="text"
-                value={wishes}
-                onChange={(e) => setWishes(e.target.value)}
-                className="w-full border-b border-red-500 p-2 outline-0"
-                placeholder="Например: 10 человек, 15000₽"
-              />
-            </div>
-          </div>
-
-          <div className="mb-4">
-            <label className="block text-sm font-medium text-gray-700 mb-2">
-              Телефон *
-            </label>
-            <div className='flex'>
-              <div className='w-8 mr-0 block p-2 pr-0 border-b border-red-500 bg-[#151515]'>
-                <p className={`${phone.length < 1 ? 'text-gray-500' : 'text-white'} mb-[1px]`}>+7</p>
-              </div>
-              <input
-                ref={inputRef}
-                value={phone}
-                onChange={(e) => setPhone(e.target.value)}
-                className="block w-full border-b border-red-500 p-2 pl-0 outline-0"
-                placeholder="888 123 45 67"
-                inputMode="numeric"
-              />
-            </div>
-          </div>
-
-          <div className="mb-4">
-            <label className="block text-sm font-medium text-gray-700 mb-2">
-              Имя *
-            </label>
-            <input
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              className="w-full border-b border-red-500 p-2 outline-0"
-              placeholder="Ваше Имя"
+            <Calendar
+              id="datePicker"
+              placeholder="Выберите дату"
+              value={date}
+              onChange={(e) => setDate(e.value ?? null)}
+              showIcon
+              locale="ru"
+              dateFormat="dd MM yy"
             />
           </div>
-
-          <button
-            disabled={isBtnDisabled}
-            onClick={async () => {
-              await sendOrder({
-                name,
-                phone,
-                date,
-                wishes
-              });
-              setName('');
-              setPhone('');
-              setDate(null);
-              setWishes('');
-              onFormClose()
-            }}
-            className={`w-full text-white py-3 rounded-md mt-4 ${
-              isBtnDisabled ? 'bg-red-300 cursor-not-allowed' : 'bg-red-500'
-            }`}
-          >
-            Отправить
-          </button>
         </div>
       </div>
+
+      <div className="mb-4">
+        <label className="block text-sm font-medium text-gray-700 mb-2">
+          Количество персон и бюджет *
+        </label>
+        <input
+          type="text"
+          value={wishes}
+          onChange={(e) => setWishes(e.target.value)}
+          className="w-full border-b border-red-500 p-2 outline-0"
+          placeholder="Например: 10 человек, 15000₽"
+        />
+      </div>
     </div>
+
+    <div className="mb-4">
+      <label className="block text-sm font-medium text-gray-700 mb-2">
+        Телефон *
+      </label>
+      <div className='flex'>
+        <div className='w-8 mr-0 block p-2 pr-0 border-b border-red-500 bg-[#151515]'>
+          <p className={`${phone.length < 1 ? 'text-gray-500' : 'text-white'} mb-[1px]`}>+7</p>
+        </div>
+        <input
+          ref={inputRef}
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+          className="block w-full border-b border-red-500 p-2 pl-0 outline-0"
+          placeholder="888 123 45 67"
+          inputMode="numeric"
+        />
+      </div>
+    </div>
+
+    <div className="mb-4">
+      <label className="block text-sm font-medium text-gray-700 mb-2">
+        Имя *
+      </label>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        className="w-full border-b border-red-500 p-2 outline-0"
+        placeholder="Ваше Имя"
+      />
+    </div>
+
+    <button
+      disabled={isBtnDisabled}
+      onClick={async () => {
+        await sendOrder({
+          name,
+          phone,
+          date,
+          wishes
+        });
+        setName('');
+        setPhone('');
+        setDate(null);
+        setWishes('');
+        onFormClose()
+      }}
+      className={`w-full text-white py-3 rounded-md mt-4 ${
+        isBtnDisabled ? 'bg-red-300 cursor-not-allowed' : 'bg-red-500'
+      }`}
+    >
+      Отправить
+    </button>
+  </div>
+</div>
   )
 }
 
--- app/components/Main/Main.tsx
+++ app/components/Main/Main.tsx
@@ -2,9 +2,15 @@
 import Image from 'next/image'
 import React, { useState } from 'react'
 import Form from '../Form/Form';
+import { Dialog } from 'primereact/dialog';
+import { handleShow, position } from '@/app/common/positions';
 
 const Main = () => {
   const [isFormOpen, setIsFormOpen] = useState(false);
+
+    const handleShowModal = () => {
+      handleShow(setIsFormOpen)
+    }
   return (
     <section className='relative w-full h-[100vh] bg-black/40 shadow-[inset_0_-10px_15px_rgba(0,0,0,0.5)]'>
       <div className="fixed inset-0 -z-10">
@@ -24,9 +30,25 @@ const Main = () => {
         <h1 className='text-3xl md:text-4xl text-white font-bold shadow-3xl text-center'>по ЮБК</h1>
         <h1 className='text-3xl md:text-4xl text-white font-bold shadow-3xl text-center'>и Симферополю</h1>
 
-        <button onClick={() => setIsFormOpen(true)} className='p-4 bg-amber-700 text-amber-50 shadow-lg font-bold rounded-md mt-20 hover:bg-amber-800 transition-all cursor-pointer'>Рссчитать фуршет</button>
+        <button onClick={handleShowModal} className='p-4 bg-amber-700 text-amber-50 shadow-lg font-bold rounded-md mt-20 hover:bg-amber-800 transition-all cursor-pointer'>Рссчитать фуршет</button>
       </div>
-      {isFormOpen && <Form onFormClose={() => setIsFormOpen(false)} />}
+      
+      {isFormOpen && (
+        <Dialog
+          header="Заполните форму, и мы вам перезвоним!"
+          blockScroll={true}
+          visible={isFormOpen}
+          position={position}
+          style={{ width: '100vw', maxWidth: '512px', minHeight: '70vh', margin: 0 }}
+          contentStyle={{ backgroundColor: '#151515' }}
+          onHide={() => {
+            if (!isFormOpen) return
+            setIsFormOpen(false)
+          }}
+          draggable={false}
+          resizable={false}
+        ><Form onFormClose={() => setIsFormOpen(false)} />
+          </Dialog>)}
     </section>
   )
 }

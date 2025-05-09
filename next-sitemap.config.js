6c317b2b3b6960dba509edb25e5e13a0342d7b12
Tigran <41443306+septoon@users.noreply.github.com>
2025-05-09 07:20:06 +0300

    update sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,6 +2,18 @@
 module.exports = {
   siteUrl: 'https://tatti-shef.ru',
   generateRobotsTxt: true,
+  outDir: './public',
+  additionalPaths: async (config) => {
+    const now = new Date().toISOString();
+    return [
+      { loc: '/', lastmod: now },
+      { loc: '/menu', lastmod: now },
+      { loc: '/cakes', lastmod: now },
+      { loc: '/contacts', lastmod: now },
+      { loc: '/delivery', lastmod: now },
+      { loc: '/services', lastmod: now },
+    ];
+  },
   robotsTxtOptions: {
     policies: [{ userAgent: '*', allow: '/' }],
   },
